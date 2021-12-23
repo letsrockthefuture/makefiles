51ad8d6060c9d84619b24a88b1571a8e2d61446a
Daniel Vincenzi <danielvincenzi@icloud.com>
2021-12-23 14:22:26 -0300

    Initial commit

--- bin/install.sh
+++ bin/install.sh
@@ -3,12 +3,13 @@
 export MAKEFILES_ORGANIZATION=${1:-letsrockthefuture}
 export MAKEFILES_PROJECT=${2:-makefiles}
 export MAKEFILES_BRANCH=${3:-main}
+export MAKEFILES=".${MAKEFILES_PROJECT}"
 export GITHUB_REPOSITORY="https://github.com/${MAKEFILES_ORGANIZATION}/${MAKEFILES_PROJECT}.git"
 
-if [ ".${MAKEFILES_PROJECT}" ] && [ -d ".${MAKEFILES_PROJECT}" ]; then
-  echo "Removing existing .${MAKEFILES_PROJECT}..."
-  rm -rf ".${MAKEFILES_PROJECT}"
+if [ "${MAKEFILES}" ] && [ -d "${MAKEFILES}" ]; then
+  echo "Removing existing ${MAKEFILES}..."
+  rm -rf "${MAKEFILES}"
 fi
 
 echo "Cloning ${GITHUB_REPOSITORY}#${MAKEFILES_BRANCH}..."
-git clone -c advice.detachedHead=false --depth=1 -b $MAKEFILES_BRANCH $GITHUB_REPOSITORY .${MAKEFILES_PROJECT}
+git clone -c advice.detachedHead=false --depth=1 -b $MAKEFILES_BRANCH $GITHUB_REPOSITORY ${MAKEFILES}
