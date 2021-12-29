52307f7b09518ae64e0404c93cf78535b3b2543b
Daniel Vincenzi <danielvincenzi@icloud.com>
2021-12-29 08:16:30 -0300

    Initial commit

--- bin/init.sh
+++ bin/init.sh
@@ -3,10 +3,10 @@
 github_url="https://github.com/letsrockthefuture/makefiles.git"
 chdir=".makefiles"
 
-if [ ${chdir} ] && [ -d ${chdir} ]; then
-    echo "Removing existing ${chdir}..."
-    rm -rf ${chdir}
+if [ $chdir ] && [ -d $chdir ]; then
+    echo "Removing existing $chdir..."
+    rm -rf $chdir
 fi
 
-echo "Cloning ${github_url}#main..."
-git clone -c advice.detachedHead=false --depth=1 ${github_url} ${chdir}
+echo "Cloning $github_url#main..."
+git clone -c advice.detachedHead=false --depth=1 $github_url $chdir
--- bin/pre_commit.sh
+++ bin/pre_commit.sh
@@ -5,10 +5,10 @@ export SHELL=/bin/bash
 github_url="https://github.com/letsrockthefuture/terraform-repository-template.git"
 chdir=".terraform-repository-template"
 
-if [ ${chdir} ] && [ -d ${chdir} ]; then
-    echo "Removing existing ${chdir}..."
-    rm -rf ${chdir}
+if [ $chdir ] && [ -d $chdir ]; then
+    echo "Removing existing $chdir..."
+    rm -rf $chdir
 fi
 
-echo "Cloning ${github_url}#main..."
-git clone -c advice.detachedHead=false --depth=1 ${github_url} ${chdir}
+echo "Cloning $github_url#main..."
+git clone -c advice.detachedHead=false --depth=1 $github_url $chdir
